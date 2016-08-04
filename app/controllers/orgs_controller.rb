cfb62a7af2a1abdc108c00ceb490c37a6485263a
ysgorilla <yumio@gorillaanalytics.com>
2016-08-04 14:13:55 -0400

    added edit/update to org

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -8,5 +8,38 @@ class OrgsController < ApplicationController
   	@reviews = @org.reviews
   end 
 
+  def new
+  	@org = Org.new
+  end
+
+  def create
+  	@org = Org.new(org_params)
+  	if @org.save
+  		redirect_to '/orgs'
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+    @org = Org.find(params[:id])
+  end
+
+  def update
+    @org = Org.find(params[:id])
+    if @org.update_attributes(org_params)
+      redirect_to @org
+    else
+      render 'edit'
+    end 
+  end
+
+
+
+private
+	def org_params
+		params.require(:org).permit(:org_name, :org_street, :org_city, :org_state, :org_zip, :org_phone, :org_url, :org_email, :org_description, :org_imageurl, :org_category, :org_subcategory, :org_tag, :org_kidgrade_start, :org_kidgrade_end, :org_featured)
+	end
+
 
 end
